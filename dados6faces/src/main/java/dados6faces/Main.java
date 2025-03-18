1f3dd0abb816a949c5666008e9a7684f4410fb25
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-18 16:43:25 -0300

    Terceiro Commit

--- dados6faces/src/main/java/dados6faces/Main.java
+++ dados6faces/src/main/java/dados6faces/Main.java
@@ -3,10 +3,45 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random numeroAleatorio = new Random();
-        int face = numeroAleatorio.nextInt(1,6);
-
         
+        Random numeroAleatorio = new Random();
+
+
+        int i;
+        int contador1 = 0;
+        int contador2 = 0;
+        int contador3 = 0;
+        int contador4 = 0;
+        int contador5 = 0;
+        int contador6 = 0;
+
+
+        for (i = 0; i <= 100; i++){
+
+            int face = numeroAleatorio.nextInt(1,6);
+            switch(face)
+            {
+                case 1: 
+                    contador1++;
+                    break;
+                case 2:
+                    contador2++;
+                    break;
+                case 3:
+                    contador3++;
+                    break;
+                case 4:
+                    contador4++;
+                    break;
+                case 5:
+                    contador5++;
+                    break;
+                case 6:
+                    contador6++;
+                    break;
+            }
+        }
+
     }
 
 
