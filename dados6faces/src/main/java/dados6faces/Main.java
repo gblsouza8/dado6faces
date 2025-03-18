81d12875678bf6368a89481ce8393c957eae803f
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-18 16:38:16 -0300

    Segundo Commit

--- dados6faces/src/main/java/dados6faces/Main.java
+++ dados6faces/src/main/java/dados6faces/Main.java
@@ -3,7 +3,10 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random numeroAleatorio = new Random();
+        int face = numeroAleatorio.nextInt(1,6);
+
+        
     }
 
 
